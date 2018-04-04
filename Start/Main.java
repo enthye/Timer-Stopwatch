e7c941f0a2ebc3033fda7d9e72086c1e9ab4a9b2
enthye <37763349+enthye@users.noreply.github.com>
2018-04-04 16:10:53 +1200

    Add files via upload

---
+++ Start/Main.java
@@ -0,0 +1,17 @@
+package TimerStopwatch.Start;
+
+import TimerStopwatch.GUI.UserInterface;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+
+/**
+ *
+ * @author enthye
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {  
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        SwingUtilities.invokeLater(new UserInterface());
+    }
+}
--- TimerLogic/TimerCountup.java
+++ TimerLogic/TimerCountup.java
@@ -1,8 +1,26 @@
+/*
+Timer-Stopwatch
+Copyright (C) 2018 enthye
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package TimerStopwatch.TimerLogic;
 
 /**
  * Counts upwards towards a set time.  Makes use of System.nanoTime for more
- * precision.  Uses a thread to keep track of the time.  GNU GPL v3.0
+ * precision.  Uses a thread to keep track of the time.
  * It time output defaults to seconds.  Provides methods to start, stop and reset
  * the timer.
  * 
